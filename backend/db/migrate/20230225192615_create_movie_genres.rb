0fab969d7ead2759a317065573f7c24bb4949b6b
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-02-26 01:03:38 +0500

    explicitly forgin_key is set to true.

--- backend/db/migrate/20230225192615_create_movie_genres.rb
+++ backend/db/migrate/20230225192615_create_movie_genres.rb
@@ -1,8 +1,8 @@
 class CreateMovieGenres < ActiveRecord::Migration[7.0]
   def change
     create_table :movie_genres do |t|
-      t.belongs_to :movie
-      t.belongs_to :genre
+      t.belongs_to :movie, forgin_key: true
+      t.belongs_to :genre, forgin_key: true
       t.timestamps
     end
     add_index :movie_genres, %i[movie_id genre_id], unique: true
