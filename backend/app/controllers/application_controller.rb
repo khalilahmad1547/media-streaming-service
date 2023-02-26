c85c00ea958211ac108ebd0c8c36c15575f17ae2
Abdur Rahman <khalil.ahmad1547@gmail.com>
2023-02-26 17:07:56 +0500

    resource not found error is handled.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -5,10 +5,15 @@ class ApplicationController < ActionController::API
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
 
   def user_not_authorized
     render json: { message: 'You are not allowed for this action' }, status: :unauthorized
+  end
+
+  def not_found
+    render json: { message: 'Resource not found' }, status: :not_found
   end
 end
